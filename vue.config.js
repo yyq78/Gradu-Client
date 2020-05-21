bfaf346e69eafdbfd80651d451ce281f9f9ca175
unknown <yuki2072@163.com>
2020-05-21 09:05:49 +0800

    修改网站图标

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,17 @@ module.exports = {
         .plugin('html')
         .tap(args => {
           args[0].title = '企业办公设备管理系统';
-          return args
+          return args;
         })
+    },
+     // 以下是pwa配置
+     pwa: {
+      iconPaths: {
+        favicon32     : 'faviconfs.png',
+        favicon16     : 'faviconfs.png',
+        appleTouchIcon: 'faviconfs.png',
+        maskIcon      : 'faviconfs.png',
+        msTileImage   : 'faviconfs.png'
+      }
     }
   }
